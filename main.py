38b492b4693d28bbf2ea840e95f532f1d3f1f88b
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-22 13:06:36 +0100

    adding and compliting tasks added

--- main.py
+++ main.py
@@ -1,50 +1,103 @@
-import tkinter as tk
-import requests
 from todoist.api import TodoistAPI
-from todoist.models import Item
 from datetime import datetime
+import emoji
+import click
+import configparser
+from tabulate import tabulate
 
-def getClient():
-    token = 'baaa494629a69edc5e8274e9be151ef34f3ce6ae'
-    client = TodoistAPI(token)
-    return client
+class TodoistConsoleClient:
 
-def getTodaysTasks():
-    client = getClient()
-    todays_date = datetime.today().strftime('%Y-%m-%d')
-    todays = [ ]
-    for item in client.state['items']:
-        if item.data['due'] != None and item.data['due']['date']  == todays_date:
-            todays.append( item.data )
-    return todays 
+    def __init__(self):
+        self.CONFIG = self.getConfig()
+        self.client = self.getClient()
+        self.DONE_EMOJI = self.CONFIG['emoji']['DONE_EMOJI']
+        self.NOT_DONE_EMOJI = self.CONFIG['emoji']['NOT_DONE_EMOJI']
 
-class App(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.pack()
+    def getClient(self):
+        client = TodoistAPI(self.CONFIG['auth']['token'])
+        client.sync()
+        return client
 
-        self.entrythingy = tk.Entry()
-        self.entrythingy.pack()
+    def getTodaysTasks(self):
+        todays = [ ]
+        for item in self.client.state['items']:
+            if self.isOverdue(item) or self.isToday(item):
+                todays.append(item.data)
+        return todays
+    
+    def isToday(self, item):
+        todays_date = datetime.today().strftime('%Y-%m-%d')
+        return item.data['due'] != None and item.data['due']['date'] == todays_date
 
-        # Create the application variable.
-        self.contents = tk.StringVar()
-        # Set it to some value.
-        self.contents.set("this is a variable")
-        # Tell the entry widget to watch this variable.
-        self.entrythingy["textvariable"] = self.contents
+    def isOverdue(self, item):
+        todays_date = datetime.today().strftime('%Y-%m-%d')
+        return item.data['due'] != None and item.data['due']['date'] <= todays_date and item.data['checked'] == 0
 
-        # Define a callback for when the user hits return.
-        # It prints the current value of the variable.
-        self.entrythingy.bind('<Key-Return>',
-                             self.print_contents)
+    def setToken(self, value):
+        self.CONFIG['auth']['token'] = value
+        with open('config.cfg', 'w') as configfile:
+            self.CONFIG.write(configfile)
+        print(f'token set to {value}')
 
-    def print_contents(self, event):
-        print("Hi. The current entry content is:",
-              self.contents.get())
+    def getConfig(self, path = 'config.cfg'):
+        config = configparser.ConfigParser()
+        config.read(path)
+        return config
 
-# root = tk.Tk()
-# myapp = App(root)
-# myapp.mainloop()
+    def printTasks(self):
+        tasks = sorted(self.getTodaysTasks(), key = lambda i: (i['due']['date']))
+        task_rows = []
+        for task in tasks: 
+            status = self.DONE_EMOJI if task['checked'] == 1 else self.NOT_DONE_EMOJI
+            task_rows.append([emoji.emojize(status), task['id'], task['content'], task['due']['string']])
+
+        headers = ['Done', 'ID', 'Task', 'Date']
+        print(tabulate(task_rows, headers=headers, showindex="always"))
+        
+    def addNewTask(self, value, date):
+        due = {
+            "date" : date
+        }
+        newitem = self.client.items.add(value,due = due)
+        self.client.commit()
+
+    def getTaskById(self, id):
+        for item in self.client.state['items']:
+            if item.data['id'] == int(id):
+                return item
+        
+    def completeTask(self, task_id):
+        item = self.getTaskById(task_id)
+        item.complete()
+        self.client.commit()
+        print(f'Task {task_id} completed')
+
+@click.command()
+@click.option(
+    "--set-token", default=None, required=False
+)
+@click.option(
+    "--add", default=False, required=None
+)
+@click.option(
+    "--date", default=lambda : datetime.today().strftime('%Y-%m-%d'), required= False
+)
+@click.option(
+    "--complete", default=None, required=False
+)
+def main(set_token, add, date, complete):
+    client = TodoistConsoleClient()
+
+    if set_token != None:
+        client.setToken(set_token)
+        return 
+    elif add:
+        client.addNewTask(add, date)
+    elif complete:
+        client.completeTask(complete)
+    else: 
+        client.printTasks()
 
 if __name__ == "__main__":
-    tasks = getTodaysTasks()
+    main()        
+    
